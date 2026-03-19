a96ceca3476705c301df502ed3449e616f001d90
Hasini Bandara <120240305+HasiniBandara@users.noreply.github.com>
2026-03-19 17:48:28 +0530

    Add files via upload

---
+++ smart-store-backend/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    // Loads .env globally
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // Connects to PostgreSQL
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true, // Automatically loads your database models (entities)
+      synchronize: true, // Auto-creates tables, ONLY for development
+    }),
+  ],
+})
+export class AppModule {}
+
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
