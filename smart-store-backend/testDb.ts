92e0d33dd2fa514c3b99d2122e0f83a06c98a432
Hasini Bandara <120240305+HasiniBandara@users.noreply.github.com>
2026-03-19 19:01:00 +0530

    Update testDb.ts

--- smart-store-backend/testDb.ts
+++ smart-store-backend/testDb.ts
@@ -4,12 +4,12 @@ import pool from './db';
 const testConnection = async () => {
   try {
     const result = await pool.query('SELECT NOW()');
-    console.log('✅ Database connected! Current time:', result.rows[0].now);
+    console.log('Database connected! Current time:', result.rows[0].now);
   } catch (err: unknown) {
     if (err instanceof Error) {
-      console.error('❌ Database connection error:', err.message);
+      console.error('Database connection error:', err.message);
     } else {
-      console.error('❌ Unexpected error', err);
+      console.error('Unexpected error', err);
     }
   } finally {
     await pool.end();
